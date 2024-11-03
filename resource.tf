726e5059f5b5c9aab203aa9af5efecaaca9394ab
Rockydonthurtem <charrisq@hotmail.com>
2024-11-03 14:24:45 -0600

    default route table

--- resource.tf
+++ resource.tf
@@ -37,20 +37,24 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 #Terraform is smart enough to know what order to create resources
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-}
+# resource "aws_route_table" "myapp-route-table" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-igw.id
+#   }
+# }
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
 }
 
 
-resource "aws_route_table_association" "a-rtb-subnet" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id      = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
